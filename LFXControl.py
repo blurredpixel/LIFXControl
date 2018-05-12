bc197117ad7cbeb8daaf252332969fac23a4ced1
David Fries <dfries4@gmail.com>
2018-05-12 16:39:50 -0400

    ran  auto-format pep8

--- LFXControl.py
+++ LFXControl.py
@@ -1,27 +1,24 @@
 
-import requests 
+import requests
 import json
 
+
 class LIFXController():
-    
 
-     
-
-    def __init__(self,token):
+    def __init__(self, token):
         def getHeaders(self):
-            
 
             headers = {
                 "Authorization": "Bearer %s" % token,
             }
 
             return headers
-        self.response= requests.get('https://api.lifx.com/v1/lights/all', headers=getHeaders(self))
-        
-    #toggles power of passed in label and state
-    def togglePower(self,token,label,state):
+        self.response = requests.get(
+            'https://api.lifx.com/v1/lights/all', headers=getHeaders(self))
+
+    # toggles power of passed in label and state
+    def togglePower(self, token, label, state):
         def getHeaders(self):
-            
 
             headers = {
                 "Authorization": "Bearer %s" % token,
@@ -31,14 +28,14 @@ class LIFXController():
         payload = {
             "power": state,
         }
-        #debug methods
+        # debug methods
         print("Label togglePower: "+label)
         print("Label state togglePower: "+state)
-        self.response= requests.put('https://api.lifx.com/v1/lights/label:'+label+'/state',data=payload, headers=getHeaders(self))
-        
-    def changeBrightness(self,token,label,brightness):
+        self.response = requests.put(
+            'https://api.lifx.com/v1/lights/label:'+label+'/state', data=payload, headers=getHeaders(self))
+
+    def changeBrightness(self, token, label, brightness):
         def getHeaders(self):
-            
 
             headers = {
                 "Authorization": "Bearer %s" % token,
@@ -48,55 +45,55 @@ class LIFXController():
         payload = {
             "brightness": brightness,
         }
-        #debug methods
+        # debug methods
         #print("Label Changebrightness: "+label)
         #print("Brightness Level : "+ brightness)
-        self.response= requests.put('https://api.lifx.com/v1/lights/label:'+label+'/state',data=payload, headers=getHeaders(self))
+        self.response = requests.put(
+            'https://api.lifx.com/v1/lights/label:'+label+'/state', data=payload, headers=getHeaders(self))
 
-    #debug method for printing json from api
-    def printAPI(self,r):
+    # debug method for printing json from api
+    def printAPI(self, r):
         print(r)
 
-#returns raw JSON data from light API
+# returns raw JSON data from light API
     def getJSON(self):
-        data=json.loads(self.response.text)
+        data = json.loads(self.response.text)
         print(data)
         return data
 
-    #debug method
+    # debug method
     def printPowerState(self):
-        data=LIFXController.getJSON(self)
-        
-            
+        data = LIFXController.getJSON(self)
+
         for i in range(len(data)):
-            print("Label: "+data[i]['label']+" "+ "Power: "+data[i]['power'])
-            
+            print("Label: "+data[i]['label']+" " + "Power: "+data[i]['power'])
 
+    # returns a list of light IDs
 
-
-    #returns a list of light IDs
     def getLightIDs(self):
-        data=LIFXController.getJSON(self)
-        ids=[]
+        data = LIFXController.getJSON(self)
+        ids = []
         for i in range(len(data)):
-            
+
             ids.append(data[i]['id'])
 
         return ids
-    #returns a list of light labels
+    # returns a list of light labels
+
     def getLightLabels(self):
-        data=LIFXController.getJSON(self)
-        info=[]
+        data = LIFXController.getJSON(self)
+        info = []
 
         for i in range(len(data)):
             info.append(data[i]['label'])
-        
-        return info
-    #returns list of lights and basic info. mainly for debug
-    def getLightsInfo(self):
-        data=LIFXController.getJSON(self)
 
-        info=[]
+        return info
+    # returns list of lights and basic info. mainly for debug
+
+    def getLightsInfo(self):
+        data = LIFXController.getJSON(self)
+
+        info = []
 
         for i in range(len(data)):
             print("ID: "+data[i]['id'])
@@ -107,28 +104,27 @@ class LIFXController():
             info.append(data[i]['power'])
         return info
 
-    #takes in a light label and return t/f if light is on or off.
-    def getLightPowerState(self,label):
-        data=LIFXController.getJSON(self)
+    # takes in a light label and return t/f if light is on or off.
+    def getLightPowerState(self, label):
+        data = LIFXController.getJSON(self)
         for i in range(len(data)):
-            if(data[i]['label']==label):    
-                if(data[i]['power']=='on'):
+            if(data[i]['label'] == label):
+                if(data[i]['power'] == 'on'):
                     return True
                 else:
                     return False
-    #gets the current brightness of selected by label light
-    def getBrightness(self,label):
-            data=LIFXController.getJSON(self)
-            for i in range(len(data)):
-                if(data[i]['label']==label):    
-                    return data[i]['brightness']
+    # gets the current brightness of selected by label light
 
-    #debug method for printing out basic light info
+    def getBrightness(self, label):
+        data = LIFXController.getJSON(self)
+        for i in range(len(data)):
+            if(data[i]['label'] == label):
+                return data[i]['brightness']
+
+    # debug method for printing out basic light info
     def printLightInfoDebug(self):
-        data=LIFXController.getJSON(self)
+        data = LIFXController.getJSON(self)
         for i in range(len(data)):
             print("ID: "+data[i]['id'])
             print("Label: "+data[i]['label'])
             print("Power: "+data[i]['power'])
-
-    
--- LIFXGUI.py
+++ LIFXGUI.py
@@ -2,63 +2,55 @@ import LFXControl as LFX
 from appJar import gui
 
 
-
-
-
-
-
-app=gui("LFX Controller","800x600")
+app = gui("LFX Controller", "800x600")
 
 app.addLabelEntry("Token:")
-#handles posting of changed parameters for lights
+# handles posting of changed parameters for lights
+
+
 def editLight(button):
-    token=app.getEntry("Token:")
+    token = app.getEntry("Token:")
     light = LFX.LIFXController(token)
 
-    light.togglePower(token,app.getListBox("labels")[0],app.getRadioButton("power"))
-    light.changeBrightness(token,app.getListBox("labels")[0],app.getScale("brightness"))
-    #this was added to make sure a duplicate widget exception won't be thrown
-    if(button=="Exit"):
+    light.togglePower(token, app.getListBox("labels")[
+                      0], app.getRadioButton("power"))
+    light.changeBrightness(token, app.getListBox(
+        "labels")[0], app.getScale("brightness"))
+    # this was added to make sure a duplicate widget exception won't be thrown
+    if(button == "Exit"):
         app.destroySubWindow("light_edit")
+
 
 def launchWin(win):
     app.showSubWindow(win)
 
+
 def debug(button):
-    token=app.getEntry("Token:")
+    token = app.getEntry("Token:")
     light = LFX.LIFXController(token)
-    
-    
-    
-    if(button=="light_edit"):
-        app.startSubWindow("light_edit",modal=True)
-        app.addRadioButton("power","on")
-        app.addRadioButton("power","off")
-        #if the light is on set the only available option to off
-        if(light.getLightPowerState(app.getListBox("labels")[0])==True):
-            app.setRadioButton("power","off",callFunction=False)
-        #adds brightness select slider
+
+    if(button == "light_edit"):
+        app.startSubWindow("light_edit", modal=True)
+        app.addRadioButton("power", "on")
+        app.addRadioButton("power", "off")
+        # if the light is on set the only available option to off
+        if(light.getLightPowerState(app.getListBox("labels")[0]) == True):
+            app.setRadioButton("power", "off", callFunction=False)
+        # adds brightness select slider
         app.addScale("brightness")
-        app.setScaleRange("brightness",0.0,1.0,light.getBrightness(app.getListBox("labels")[0]))
-        app.addButtons(["Submit Changes","Exit"],editLight)
+        app.setScaleRange("brightness", 0.0, 1.0,
+                          light.getBrightness(app.getListBox("labels")[0]))
+        app.addButtons(["Submit Changes", "Exit"], editLight)
         app.stopSubWindow()
         app.showSubWindow("light_edit")
 
     else:
-        app.addListBox("labels",light.getLightLabels())
+        app.addListBox("labels", light.getLightLabels())
         print(light.printLightInfoDebug())
-    
-        
-    
 
 
-app.addButton("Connect",debug)
-app.addButton("light_edit",debug)
-
-
+app.addButton("Connect", debug)
+app.addButton("light_edit", debug)
 
 
 app.go()
-
-
-
